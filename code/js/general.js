1350b5cd91227658f4e5994696f8c6f99c48dc81
FrancisGrace <2208853487@qq.com>
2022-01-27 20:38:20 +0800

    初始结构

---
+++ code/js/general.js
@@ -0,0 +1,65 @@
+/**
+ * @author Enze Zhang
+ * general.js--JavaScript used on all pages
+ */
+
+/**
+ * Initialise
+ */
+function init() {
+    checkBrowser();
+    setTopScroll();
+    setNav();
+}
+
+/**
+ * Give a hint to user who uses IE
+ */
+function checkBrowser() {
+    if (navigator.userAgent.indexOf('Trident') !== -1) {
+        let mainText = document.getElementById('main-text');
+        let hint = document.createElement('div');
+        hint.setAttribute('class', 'alert alert-warning');
+        hint.innerHTML = 'For your better experience, please use Chrome or Firefox browser.';
+        document.body.insertBefore(hint, mainText);
+    }
+}
+
+/**
+ * Implement the function of top-scroll button
+ */
+function setTopScroll() {
+    let f = function () {
+        let textT = document.getElementById("main-text").offsetTop;
+        let footerT = document.getElementById("footer").offsetTop;
+        let sidebarH = document.getElementById("sidebar") ? document.getElementById("sidebar").clientHeight : 0;
+        let currentH = document.documentElement.scrollTop || document.body.scrollTop;
+        let topScroll = document.getElementById("top-scroll");
+        if (currentH > textT - 130 && currentH + sidebarH < footerT - 130) {
+            topScroll.setAttribute("class", "top-scroll-active");
+        } else {
+            topScroll.setAttribute("class", "");
+        }
+    }
+    window.addEventListener('resize', f);
+    window.addEventListener('scroll', f);
+    let ts = document.getElementById('top-scroll').childNodes[1];
+    ts.addEventListener('click', function () {
+        scroll({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        });
+    });
+}
+
+/**
+ * Set the top of the navigation bar
+ */
+function setNav() {
+    let igemNav = document.getElementById('top_menu_14');
+    let h = '0';
+    if (igemNav)
+        h = igemNav.offsetHeight + 'px';
+    document.getElementById('nav-bar').style.top = h;
+}
---
+++ code/js/sidebar.js
@@ -0,0 +1,101 @@
+/**
+ * @author Enze Zhang
+ * sidebar.js--JavaScript used on pages with long articles and a sidebar
+ */
+
+new Vue({
+    el: '#sidebar',
+    data: {
+        isActive: false,
+        articles: document.querySelectorAll('article>h1,article>h2.marked'),
+        articleArray: document.getElementsByClassName('article'),
+        canvasArray: document.getElementsByClassName('canvas'),
+        itemArray: document.getElementsByClassName('side-item'),
+        itemActive: []
+    },
+    methods: {
+        //Update the sidebar
+        pageView: function (canvas, index, article) {
+            let navH = document.getElementById('nav-bar').offsetHeight;
+            // console.log(this.itemActive);
+            //Something about an article
+            let elementH = article.clientHeight;
+            let currentH = document.documentElement.scrollTop || document.body.scrollTop;
+            let elementT = article.offsetTop;
+            let clientH = document.documentElement.clientHeight;
+
+            //Calculate the reading progress
+            let scale = (currentH - elementT + navH + 20) / elementH;/*看顶部*/
+            // let scale = (currentH + clientH - elementT) / elementH;/*看底部*/
+
+            //Change the styles when you start reading a new article
+            if (scale >= 0 && scale <= 1) {
+                this.$set(this.itemActive, index, true);
+            } else {
+                this.$set(this.itemActive, index, false);
+            }
+
+            //Clear the canvas
+            let ctx = canvas.getContext('2d');
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+            //Foreground
+            if (scale >= 0) {
+                ctx.beginPath();
+                ctx.lineWidth = 3;
+                ctx.arc(20, 20, 10, this.d2a(-90), this.d2a(scale * 360 - 90), false)
+                ctx.strokeStyle = '#397689';
+                ctx.stroke();
+            }
+        },
+        //Convert angles to degrees
+        d2a: function (n) {
+            return n * Math.PI / 180;
+        },
+    },
+    mounted: function () {
+        let that = this;
+        //Initialise itemActive
+        for (let i = 0; i < that.itemArray.length; i++)
+            that.itemActive.push(false);
+        window.addEventListener('load', function () {
+            //Give an ID for each title of an article
+            for (let i = 0; i < that.articleArray.length; i++) {
+                that.articleArray[i].setAttribute('id', 'article' + (i + 1));
+            }
+            //Generate the scrolling effect of the sidebar
+            let navHeight = document.getElementById('nav-bar').offsetHeight;
+            for (let i = 0; i < that.itemArray.length; i++) {
+                that.itemArray[i].onclick = function () {
+                    scroll({
+                        top: that.articleArray[i].offsetTop - navHeight + 5,
+                        //To prevent the overshadow of the navigation bar
+                        left: 0,
+                        behavior: 'smooth'
+                    });
+                    return false;
+                };
+                //Click the items to scroll
+                that.pageView(that.canvasArray[i], i, that.articleArray[i]);//Update
+            }
+        })
+
+        let f = function () {
+            //Respond changes when user scrolls or resizes
+            for (let i = 0; i < that.canvasArray.length; i++) {
+                that.pageView(that.canvasArray[i], i, that.articleArray[i]);//Update
+            }
+            let textT = document.getElementById("main-text").offsetTop;
+            let footerT = document.getElementById("footer").offsetTop;
+            let sidebarH = document.getElementById("sidebar").clientHeight;
+            let currentH = document.documentElement.scrollTop || document.body.scrollTop;
+            if (currentH > textT - 130 && currentH + sidebarH < footerT - 130)
+                that.isActive = true;
+            else
+                that.isActive = false;
+        };
+
+        window.addEventListener('resize', f);
+        window.addEventListener('scroll', f);
+    }
+})
